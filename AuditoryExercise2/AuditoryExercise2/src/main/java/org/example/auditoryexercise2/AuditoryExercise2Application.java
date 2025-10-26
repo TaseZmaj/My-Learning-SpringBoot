a9ec050984cc81d364659845c1fdbfd7eceeba00
TaseZmaj <stefan.tasevski.2@students.finki.ukim.mk>
2025-10-26 15:02:35 +0100

    Created a new project

---
+++ AuditoryExercise2/AuditoryExercise2/src/main/java/org/example/auditoryexercise2/AuditoryExercise2Application.java
@@ -0,0 +1,13 @@
+package org.example.auditoryexercise2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AuditoryExercise2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AuditoryExercise2Application.class, args);
+    }
+
+}
---
+++ AuditoryExercise2/AuditoryExercise2/src/test/java/org/example/auditoryexercise2/AuditoryExercise2ApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.auditoryexercise2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AuditoryExercise2ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
